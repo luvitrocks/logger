c727a4220360f48d09a52e064d4a5c0e300aca91
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-07-22 21:50:51 +0300

    add package.lua

---
+++ package.lua
@@ -0,0 +1,15 @@
+-- for older versions of luvit and alternative package managers
+return {
+	name = "luvit-logger",
+	version = "0.0.2",
+	description = "http request logger middleware for luvit.io",
+	repository = {
+		url = "http://github.com/luvitrocks/luvit-logger.git",
+	},
+	author = {
+		name = "Dmitri Voronianski",
+		email = "voronianski@gmail.com"
+	},
+	licenses = {"MIT"},
+	main = 'init.lua'
+}
